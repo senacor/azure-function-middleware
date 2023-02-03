0e6e6e231f6516527935672d8db1341fcdba4338
frudisch@gmx.de <frudisch@gmx.de>
2023-02-03 22:13:57 +0100

    Updated integration test configuration to include axios

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -4,10 +4,12 @@ module.exports = {
     testMatch: [
         '**/*.integration.ts'
     ],
-    testTimeout: 10000,
     testPathIgnorePatterns: ["/node_modules/", "/dist/", "/config/"],
     moduleDirectories: [
         "node_modules"
     ],
-    transformIgnorePatterns: ["node_modules/(?!axios)"],
+    testTimeout: 10000,
+    moduleNameMapper: {
+        "axios": "axios/dist/node/axios.cjs"
+    }
 }
